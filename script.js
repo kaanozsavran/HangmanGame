11aa87a340498f9ab452a8e8a20f00b4ce3812b0
kaanozsavran <kaanozsavran@hotmail.com>
2025-01-14 22:55:10 +0300

    add selectedWord properties

---
+++ script.js
@@ -0,0 +1,35 @@
+const word_el = document.getElementById('word');
+
+
+const correctLetters = ['j', 'a', 'v', 's'];
+const wrongLetters = [];
+
+
+function getRandomWord() {
+    const words = ["javascript", "java", "python"];
+    return words[Math.floor(Math.random() * words.length)];
+}
+
+
+
+
+
+function displayWord() {
+    const selectedWord = getRandomWord();
+
+    word_el.innerHTML = `
+        ${selectedWord.split('').map(letter => `
+            <div class ="letter"> 
+                ${correctLetters.includes(letter) ? letter : ''}
+                </div>
+            
+            `).join('')}
+    
+    `;
+
+    const w = word_el.innerText.replace(/\n/g, '');
+    if (w === selectedWord) {
+        console.log("bildiniz.")
+    }
+}
+displayWord();
